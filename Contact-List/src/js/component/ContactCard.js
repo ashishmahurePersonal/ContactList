ec2063da1a2b498f18dddb0b8d1c2d9e46f9432e
Ashish Mahure <ashish.mahure@oracle.com>
2019-07-31 02:43:10 +0530

    ui chnages

---
+++ Contact-List/src/js/component/ContactCard.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+class ContactCard extends React.Component {
+	render() {
+		return (
+			<li className="list-group-item">
+				<div className="row w-100">
+					<div className="col-12 col-sm-6 col-md-2 px-0">
+						<img
+							src={this.props.image_url}
+							alt={this.props.fullname}
+							className="rounded-circle mx-auto d-block img-fluid"
+						/>
+					</div>
+					<div className="col-12 col-sm-6 col-md-9 text-center text-sm-left">
+						<div className=" float-right">
+							<Link to={"/edit/" + this.props.email + "/" + this.props.rowIndex}>
+								<button className="btn">
+									<i className="fas fa-pencil-alt mr-3" />
+								</button>
+							</Link>
+							<Link to="/">
+								<button className="btn" onClick={() => this.props.onDelete()}>
+									<i className="fas fa-trash-alt" />
+								</button>
+							</Link>
+						</div>
+						<strong className="name lead">{this.props.fullname}</strong>
+						<span
+							className="fa fa-envelope fa-fw text-muted mr-3"
+							data-toggle="tooltip"
+							data-original-title=""
+							title=""
+						/>
+						<span className="text-muted small text-truncate">{this.props.email}</span>
+						<br />
+						<i className="fas fa-map-marker-alt text-muted mr-3" />
+						<span className="text-muted">{this.props.address}</span>
+						<br />
+						<span
+							className="fa fa-phone fa-fw text-muted mr-3"
+							data-toggle="tooltip"
+							title=""
+							data-original-title="(870) 288-4149"
+						/>
+						<span className="text-muted small">{this.props.phone}</span>
+					</div>
+				</div>
+			</li>
+		);
+	}
+}
+
+/**
+ * Define the data-types for
+ * your component's properties
+ **/
+ContactCard.propTypes = {
+	fullname: PropTypes.string,
+	email: PropTypes.string,
+	phone: PropTypes.string,
+	address: PropTypes.string,
+	image_url: PropTypes.string,
+	history: PropTypes.object,
+	onDelete: PropTypes.func,
+	rowIndex: PropTypes.any
+};
+
+/**
+ * Define the default values for
+ * your component's properties
+ **/
+ContactCard.defaultProps = {
+	onDelete: null
+};
+export default ContactCard;
